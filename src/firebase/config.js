17a6729991bb79ce316f6dc05c7f7e02b3855e72
Gonzalo Billia <gonzalobillia@gmail.com>
2024-03-14 21:23:06 -0300

    añado config de firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket:import.meta.env. VITE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_APP_ID
+    apiKey: "AIzaSyAXWGbff01VBvZ9QjGso_Hyg7oyadIMEzY",
+    authDomain: "proyecto-final-react-cod-c21ca.firebaseapp.com",
+    projectId: "proyecto-final-react-cod-c21ca",
+    storageBucket:"proyecto-final-react-cod-c21ca.appspot.com",
+    messagingSenderId: "581980427535",
+    appId: "1:581980427535:web:bb2f1821ba42bd8fa7da7a"
 }
 
 const app = initializeApp(firebaseConfig);
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://GonzaBillia.github.io/Proyecto-React-REGAHARD/',
+  base: 'https://gonzabillia.github.io/Proyecto-React-REGAHARD/',
 })
