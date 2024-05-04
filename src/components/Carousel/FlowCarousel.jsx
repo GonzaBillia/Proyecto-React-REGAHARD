4341dfad38d9b730d73b7db067cff1637ca9ddea
Gonzalo Billia <gonzalobillia@gmail.com>
2024-05-04 15:52:05 -0300

    coloco imagenes en carrusel (pendiente a mejorar)

--- src/components/Carousel/FlowCarousel.jsx
+++ src/components/Carousel/FlowCarousel.jsx
@@ -4,19 +4,38 @@ import { Carousel } from "@material-tailwind/react"
 export default function FlowCarousel() {
     return (
         <div className='pt-28'>
-            <Carousel className="h-96">
+            <Carousel className="h-auto" autoplay={true} loop={true}
+            navigation={({ setActiveIndex, activeIndex, length }) => (
+                <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+                    {new Array(length).fill("").map((_, i) => (
+                        <span
+                        key={i}
+                        className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
+                            activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
+                        }`}
+                        onClick={() => setActiveIndex(i)}
+                        />
+                    ))}
+                </div>
+            )}
+            >
                 <img
-                    src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+                    src="https://firebasestorage.googleapis.com/v0/b/proyecto-final-react-cod-c21ca.appspot.com/o/aorus.jpg?alt=media&token=8022fa32-fbcd-4e80-8f60-8b1540166be7"
                     alt="image 1"
                     className="h-full w-full object-cover"
                 />
                 <img
-                    src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+                    src="https://firebasestorage.googleapis.com/v0/b/proyecto-final-react-cod-c21ca.appspot.com/o/asus.jpg?alt=media&token=a61d9810-1c34-4917-90e5-cecbc993251b"
                     alt="image 2"
                     className="h-full w-full object-cover"
                 />
                 <img
-                    src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+                    src="https://firebasestorage.googleapis.com/v0/b/proyecto-final-react-cod-c21ca.appspot.com/o/corsair.jpg?alt=media&token=c672ab3b-e8df-4a46-97e3-587ddb828f31"
+                    alt="image 3"
+                    className="h-full w-full object-cover"
+                />
+                <img
+                    src="https://firebasestorage.googleapis.com/v0/b/proyecto-final-react-cod-c21ca.appspot.com/o/fury.jpg?alt=media&token=147f0f55-ede4-4770-9305-403d41a60e5d"
                     alt="image 3"
                     className="h-full w-full object-cover"
                 />
